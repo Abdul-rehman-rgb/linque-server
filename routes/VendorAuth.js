94b8a060bedfdb377963d936ad4bdc81831464f7
HIBBANQURESHI <qureshihibban@gmail.com>
2025-08-25 23:32:57 +0500

    added

--- routes/VendorAuth.js
+++ routes/VendorAuth.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { login, signup, verify } from "../controllers/authController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { login, signup, verify } from "../controllers/VendorAuthController.js";
+import vendorMiddleware from "../middleware/vendorAuth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
-router.get("/verify", authMiddleware, verify);
+router.get("/verify", vendorMiddleware, verify);
 
 export default router;
